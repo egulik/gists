6a818824ea60dd0e0aa6567a16d59a6bcc5facec
egulik <egulik@users.noreply.github.com>
2018-08-07 10:22:55 -0500

    Update get-all-vpcs-and-cidrs.sh

--- aws/get-all-vpcs-and-cidrs.sh
+++ aws/get-all-vpcs-and-cidrs.sh
@@ -6,7 +6,7 @@ REGIONS=$(aws ec2 describe-regions --region us-west-2 --query "Regions[].RegionN
 echo "Listing non default VPC's"
 for region in $REGIONS
    do
-      echo "Getting Custom VPC list in $region region:"
+      echo "vpcs & cidrs in $region region:"
       aws ec2 describe-vpcs --filters Name=isDefault,Values=false --query "Vpcs[].{id:VpcId,cidr:CidrBlock}" --region=${region} --output table | sed -e '1,2d'
    done
 
@@ -15,6 +15,6 @@ echo -e "-----\n------"
 echo "Listing default VPCS:"
 for region in $REGIONS
    do
-      echo "Getting Custom VPC list in $region region:"
+      echo "vpcs & cidrs in $region region:"
       aws ec2 describe-vpcs --filters Name=isDefault,Values=true --query "Vpcs[].{id:VpcId,cidr:CidrBlock}" --region=${region} --output table | sed -e '1,2d'
    done
